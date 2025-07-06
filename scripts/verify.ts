1ba36ce75a74d53c1c6ddc4f45e67489c3b30b73
aspnxdd <arnauespin@gmail.com>
2025-07-06 03:43:21 +0200

    feat: add verify

---
+++ scripts/verify.ts
@@ -0,0 +1,19 @@
+import { run } from "hardhat";
+
+export async function verify(contractAddress: string, args: any[] = []) {
+  console.log("🔍 Verifying contract...");
+  
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error: any) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("✅ Contract is already verified!");
+    } else {
+      console.log("❌ Verification failed:", error.message);
+      throw error;
+    }
+  }
+} 
